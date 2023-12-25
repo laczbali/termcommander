eec21c5021a121f02491eb9a66a5f54a0fa2d192
LaczkóBalázs <lab@frankly.dk>
2023-12-25 17:48:04 +0100

    adding comments, moving window init to separate method

--- termcommander/Layout/NcWindow.cs
+++ termcommander/Layout/NcWindow.cs
@@ -4,101 +4,139 @@ using Mindmagma.Curses;
 namespace ConsoleApp.Layout;
 public abstract class NcWindow : IDisposable
 {
-	private static bool colorsInitialized = false;
+    private static bool colorsInitialized = false;
 
-	protected readonly WindowSize size;
-	protected IntPtr windowObj;
+    protected readonly WindowSize size;
+    protected IntPtr windowObj;
 
-	public NcWindow(WindowSize size)
-	{
-		InitColors();
+    public NcWindow(WindowSize size)
+    {
+        this.size = size;
+        InitColors();
+        Init();
+    }
 
-		this.size = size;
+    /// <summary>
+    /// Draws the window border and title
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="on"></param>
+    public void ToggleBox(string? title = null, bool on = true)
+    {
+        if (on)
+        {
+            NCurses.WindowBorder(windowObj, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0);
+            if (title is not null)
+            {
+                NCurses.MoveWindowAddString(windowObj, 0, 2, $" {title} ");
+            }
+        }
+        else
+        {
+            NCurses.WindowBorder(windowObj, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+        }
 
-		windowObj = NCurses.NewWindow(size.Rows, size.Columns, size.RowOrigin, size.ColumnsOrigin);
-		NCurses.Keypad(windowObj, true);
-		NCurses.NoDelay(windowObj, true);
-	}
+        NCurses.WindowRefresh(windowObj);
+    }
 
-	public void ToggleBox(string? title = null, bool on = true)
-	{
-		if (on)
-		{
-			NCurses.WindowBorder(windowObj, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0, (char)0);
-			if (title is not null)
-			{
-				NCurses.MoveWindowAddString(windowObj, 0, 2, $" {title} ");
-			}
-		}
-		else
-		{
-			NCurses.WindowBorder(windowObj, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-		}
+    /// <summary>
+    /// Toggles the color pair on or off
+    /// </summary>
+    /// <param name="pair"></param>
+    /// <param name="on"></param>
+    public void ToggleColorPair(ColorPairs pair, bool on)
+    {
+        if (on)
+        {
+            NCurses.WindowAttributeOn(windowObj, NCurses.ColorPair((int)pair));
+        }
+        else
+        {
+            NCurses.WindowAttributeOff(windowObj, NCurses.ColorPair((int)pair));
+        }
+    }
 
-		NCurses.WindowRefresh(windowObj);
-	}
+    /// <summary>
+    /// Starts at the current cursor position and writes ' ' until the end of the line
+    /// </summary>
+    /// <param name="rightMargin"></param>
+    /// <param name="refresh"></param>
+    public void ClearRestOfLine(int rightMargin = 1, bool refresh = false)
+    {
+        NCurses.GetYX(windowObj, out _, out var col);
+        for (var i = col; i < size.Columns - rightMargin; i++)
+        {
+            NCurses.WindowAddChar(windowObj, ' ');
+        }
 
-	public void ToggleColorPair(ColorPairs pair, bool on)
-	{
-		if (on)
-		{
-			NCurses.WindowAttributeOn(windowObj, NCurses.ColorPair((int)pair));
-		}
-		else
-		{
-			NCurses.WindowAttributeOff(windowObj, NCurses.ColorPair((int)pair));
-		}
-	}
+        if (refresh)
+            NCurses.WindowRefresh(windowObj);
+    }
 
-	public void ClearRestOfLine(int rightMargin = 1, bool refresh = false)
-	{
-		NCurses.GetYX(windowObj, out _, out var col);
-		for (var i = col; i < size.Columns - rightMargin; i++)
-		{
-			NCurses.WindowAddChar(windowObj, ' ');
-		}
+    /// <summary>
+    /// Initializes the color pairs (if not already initialized)
+    /// </summary>
+    public static void InitColors()
+    {
+        if (colorsInitialized) return;
+        colorsInitialized = true;
 
-		if (refresh)
-			NCurses.WindowRefresh(windowObj);
-	}
+        NCurses.StartColor();
+        NCurses.InitPair((short)ColorPairs.BlueOnBlack, CursesColor.BLUE, CursesColor.BLACK);
+        NCurses.InitPair((short)ColorPairs.RedOnBlack, CursesColor.RED, CursesColor.BLACK);
+    }
 
-	public static void InitColors()
-	{
-		if (colorsInitialized) return;
-		colorsInitialized = true;
+    /// <summary>
+    /// If the string is longer than the max length, shorten it and add "..."
+    /// <br/>
+    /// If maxLength is null, use the window's width
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="maxLength"></param>
+    /// <returns></returns>
+    public string ShortenString(string str, int? maxLength = null)
+    {
+        // 2 chars for borders, 3 chars for left padding
+        // 3 chars for "..." if needed
+        if (maxLength is null)
+        {
+            maxLength = size.Columns - 5;
+        }
 
-		NCurses.StartColor();
-		NCurses.InitPair((short)ColorPairs.BlueOnBlack, CursesColor.BLUE, CursesColor.BLACK);
-		NCurses.InitPair((short)ColorPairs.RedOnBlack, CursesColor.RED, CursesColor.BLACK);
-	}
+        if (str.Length < maxLength) return str;
+        var shorter = str.Substring(0, (int)maxLength - 3);
+        return $"{shorter}...";
+    }
 
-	public string ShortenString(string str, int? maxLength = null)
-	{
-		// 2 chars for borders, 3 chars for left padding
-		// 3 chars for "..." if needed
-		if (maxLength is null)
-		{
-			maxLength = size.Columns - 5;
-		}
+    /// <summary>
+    /// Creates the window object, sets up keypad and no-delay
+    /// </summary>
+    private void Init()
+    {
+        if (windowObj != IntPtr.Zero)
+        {
+            throw new InvalidOperationException("Window already initialized");
+        }
 
-		if (str.Length < maxLength) return str;
-		var shorter = str.Substring(0, (int)maxLength - 3);
-		return $"{shorter}...";
-	}
+        windowObj = NCurses.NewWindow(size.Rows, size.Columns, size.RowOrigin, size.ColumnsOrigin);
+        NCurses.Keypad(windowObj, true);
+        NCurses.NoDelay(windowObj, true);
+    }
 
-	/// <summary>
-	/// Clears the popup from the screen, disposes of the window object
-	/// </summary>
-	/// <returns></returns>
-	public virtual void Dispose()
-	{
-		ToggleBox(on: false);
-		NCurses.DeleteWindow(windowObj);
-	}
+    /// <summary>
+    /// Clears the popup from the screen, disposes of the window object
+    /// </summary>
+    /// <returns></returns>
+    public virtual void Dispose()
+    {
+        ToggleBox(on: false);
+        NCurses.DeleteWindow(windowObj);
+        windowObj = IntPtr.Zero;
+    }
 
-	public enum ColorPairs
-	{
-		BlueOnBlack = 1,
-		RedOnBlack = 2,
-	}
+    public enum ColorPairs
+    {
+        BlueOnBlack = 1,
+        RedOnBlack = 2,
+    }
 }
